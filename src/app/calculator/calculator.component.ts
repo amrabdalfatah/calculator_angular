0599334ca69a7777c9c9fe179fed41107374c9a4
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2022-09-20 10:37:27 +0200

    Add Logic to Calculator Component

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -11,17 +11,79 @@ export class CalculatorComponent {
     result:string = '';
 
     // Method to clear all the input
-    allclear() {  }
+    allclear() { 
+        this.input = '';
+        this.result = '';
+     }
 
-    // method to clear the last operand
-    clear() {}
+    // method to clear the last input
+    clear() {
+        if(this.input != '') {
+            this.input = this.input.substring(0, this.input.length -1);
+        }
+    }
 
     // method to declare operator
-    pressOperator(arg0: string) {}
+    pressOperator(op: string) {
+        const lastKey = this.input[this.input.length - 1];
+        if(lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+') {
+            return;
+        }
+        this.input = this.input + op;
+        this.calcResult();
+    }
 
     // method to declare number
-    pressNum(arg0: string) {}
+    pressNum(num: string) {
+        if(num == '.') {
+            if(this.input != '') {
+                const lastNum = this.getLastOperand();
+                if(lastNum.lastIndexOf('.') >= 0) return;
+            }
+        }
+
+        if(num == '0') {
+            if(this.input == '') {
+                return ;
+            }
+            const prevKey = this.input[this.input.length - 1];
+            if(prevKey === '/' || prevKey === '*' || prevKey === '-' || prevKey === '+') {
+                return;
+            }
+        }
+        this.input = this.input + num;
+        this.calcResult();
+    }
+
+    // method to get last operand
+    getLastOperand() {
+        let pos:number;
+        pos = this.input.toString().lastIndexOf('+')
+        if (this.input.toString().lastIndexOf('-') > pos) pos = this.input.lastIndexOf('-');
+        if (this.input.toString().lastIndexOf('*') > pos) pos = this.input.lastIndexOf('*');
+        if (this.input.toString().lastIndexOf('/') > pos) pos = this.input.lastIndexOf('/');
+        return this.input.substring(pos + 1);
+    }
+
+    // method to calc result 
+    calcResult() {
+        let formula = this.input;
+        let lastKey = formula[formula.length - 1];
+        if(lastKey === '.') {
+            formula = formula.substring(0, formula.length - 1);
+        }
+        lastKey = formula[formula.length - 1];
+        if(lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+' || lastKey === '.') {
+            formula = formula.substring(0, formula.length - 1);
+        }
+
+        this.result = eval(formula);
+    }
 
     // method to get result 
-    getResult() {}
+    getResult() {
+        this.calcResult();
+        this.input = this.result;
+        if(this.input == '0') this.input='';
+    }
 }
